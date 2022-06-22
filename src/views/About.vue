9a10879f880eab778912a5158b8358c9fb4abfd9
jiangfei03 <jiangfei03@daojia-inc.com>
2022-06-22 17:03:30 +0800

    chore: add vant

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <van-button type="primary" @click="showPopup">展示弹出层</van-button>
+    <van-popup position="bottom" :style="{ height: '20%' }" v-model="show">
+      内容
+    </van-popup>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent, h, ref } from "vue";
+
+export default defineComponent({
+  name: "About",
+  setup() {
+    let show = ref(false);
+    const showPopup = () => {
+      show.value = true;
+    };
+    return { show, showPopup };
+  },
+});
+</script>
