188a29e09a8918d82ce85bc87f16545a2e35e3f7
jiangfei03 <jiangfei03@daojia-inc.com>
2022-06-27 15:58:48 +0800

    feat: router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,27 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld :msg="msg" />
     <van-button type="primary" @click="changeMsg">改变</van-button>
+    <van-button type="default" @click="goAbout">跳转About</van-button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, getCurrentInstance } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
   components: { HelloWorld },
   setup() {
+    console.log("********$route", getCurrentInstance());
     let msg = ref("hello");
     const changeMsg = () => {
       msg.value += " 12";
     };
-    return { changeMsg, msg };
+    const goAbout = () => {
+      // router.push("/about");
+    };
+    return { changeMsg, msg, goAbout };
   },
 });
 </script>
